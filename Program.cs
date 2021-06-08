bf930ae6dda7c82c7890493edbdf160e5fa725bb
ArthurWash <Arthurjwash@gmail.com>
2021-06-08 18:34:04 -0400

    Feature: Added reading or writing data in program.cs

--- Program.cs
+++ Program.cs
@@ -6,7 +6,17 @@ namespace EFCoreTutorials
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            using (var context = new SchoolContext())
+            {
+
+                var std = new Student()
+                {
+                    Name = "Bill"
+                };
+
+                context.Students.Add(std);
+                context.SaveChanges();
+            }
         }
     }
 }
