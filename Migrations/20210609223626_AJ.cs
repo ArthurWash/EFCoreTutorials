61fa331f9343c35b5a35165b4006d1628c67e600
ArthurWash <Arthurjwash@gmail.com>
2021-06-09 18:45:57 -0400

    Feature: Successfully updated the database.

---
+++ Migrations/20210609223626_AJ.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EFCoreTutorials.Migrations
+{
+    public partial class AJ : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
